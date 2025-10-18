352f007b3b14f142ce5e1ee9f3093806c2a1b158
MajdS12 <154749001+MajdS12@users.noreply.github.com>
2025-10-18 16:47:55 +0300

    Fix Railway deployment: Update nixpacks.toml and wsgi.py for Python 3.13

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from decouple import config
 
 # Use production settings if in production environment
-settings_module = config('DJANGO_SETTINGS_MODULE', default='settings')
+settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'settings')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
